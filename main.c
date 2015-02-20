1b0da219b58481734d00de9cb649af9e2e88086b
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-20 20:00:30 +0100

    Soluzione di riferimento

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+#define N 5
 
+int main(int argc, char** argv) {
+    int i;
+    int eta[N];
+    int maggiorenni;
+    
+    printf("Inserisci l'eta' di %d persone\n", N);
+    for(i=0; i<N; i++) {
+        printf("Inserisci l'eta' della %da persona: ", i+1);
+        scanf("%d", &(eta[i]));
+    }
+    
+    maggiorenni = 0;
+    for(i=0; i<N; i++) {
+        if(eta[i] >= 18) {
+            maggiorenni++;
+        }
+    }
+    
+    printf("I maggiorenni sono: %d\n", maggiorenni);
+    
     return (EXIT_SUCCESS);
 }
 
